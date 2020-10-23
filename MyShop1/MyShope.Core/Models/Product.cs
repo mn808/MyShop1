d4ff1323878005b81e6e55874a780a97fcc63068
mn808 <73227045+mn808@users.noreply.github.com>
2020-10-23 14:23:15 +0530

    Product Model and InMemory Product Repository Built

---
+++ MyShop1/MyShope.Core/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShope.Core.Models
+{
+   public class Product
+    {
+        public string Id;
+
+        [StringLength(20)]
+        public string Name;
+        public string Description;
+
+        [Range(0, 1000)]
+        public decimal Price;
+        public string Category;
+        public string Image;
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
