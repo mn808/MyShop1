5061831f5bb9044cffa201a0e76cdf00450af7b3
mn808 <73227045+mn808@users.noreply.github.com>
2020-10-25 20:26:18 +0530

    Added Product Filter

---
+++ MyShop1/MyShope.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyShope.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShope.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products;
+        public IEnumerable<ProductCategory> ProductCategories;
+    }
+}
