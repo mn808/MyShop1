628fd965646ac3aa21ab8cc4ac7a907b798f63ff
mn808 <73227045+mn808@users.noreply.github.com>
2020-10-25 14:43:39 +0530

    Injected Dependency

--- MyShop1/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
+++ MyShop1/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.DataAccess.InMemory;
+﻿using MyShop.Core.Contracts;
+using MyShop.DataAccess.InMemory;
 using MyShope.Core.Models;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,11 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductCategoryManagerController : Controller
     {
-        InMemoryRepository<ProductCategory> context;
+        IRepository<ProductCategory> context;
 
-        public ProductCategoryManagerController()
+        public ProductCategoryManagerController(IRepository<ProductCategory> context)
         {
-            context = new InMemoryRepository<ProductCategory>();
+            this.context = context;
         }
 
 
